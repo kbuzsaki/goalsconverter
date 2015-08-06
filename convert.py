df808915b3f206b17dba68049a9afcef6bfea730
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2015-08-05 20:23:53 -0700

    Updated to match new csv format

--- convert.py
+++ convert.py
@@ -4,12 +4,13 @@ import csv
 from urllib.request import urlopen
 import os
 import traceback
+import re
 
 
 class ValueColumn:
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, *path):
+        self.path = path
 
     @property
     def included(self):
@@ -25,6 +26,21 @@ class IntegerColumn(ValueColumn):
     def parse_value(self, value):
         return int(value)
 
+class FloatColumn(ValueColumn):
+
+    def parse_value(self, value):
+        return float(value)
+
+class BooleanColumn(ValueColumn):
+
+    def parse_value(self, value):
+        if value.lower() in ("true", "yes", "y"):
+            return True
+        elif value.lower() in ("false", "no", "n"):
+            return False
+        else:
+            raise ValueError("Couldn't parse value '" + str(value) + "' as boolean")
+
 class Ignore:
 
     def __init__(self, name):
@@ -35,20 +51,46 @@ class Ignore:
         return False
 
 SCHEMA = [
-    StringColumn("name"),
-    StringColumn("jp"),
-    StringColumn("difficulty"),
-    StringColumn("child")
+    Ignore("updated"),
+    StringColumn("payload", "name"),
+    StringColumn("payload", "jp"),
+    IntegerColumn("difficulty"),
+    FloatColumn("time"),
+    StringColumn("skill"),
+    BooleanColumn("child"),
+    BooleanColumn("bottle"),
+    BooleanColumn("hookshot"),
 ]
 
+NON_ID_CHARACTERS_PATTERN = re.compile(r"[^A-Za-z0-9]+")
+
+def id_from_name(name):
+    return NON_ID_CHARACTERS_PATTERN.sub("-", name.lower())
+
+def id_from_goal(goal):
+    return goal["id"] if "id" in goal else id_from_name(goal["payload"]["name"])
+
+def set_deep(dictionary, keys, value):
+    path_keys = keys[:-1]
+    last_key = keys[-1]
+    for path_key in path_keys:
+        if path_key not in dictionary:
+            dictionary[path_key] = dict()
+        dictionary = dictionary[path_key]
+    dictionary[last_key] = value
+
 def parse_goal(col_details):
-    return {col.name: col.parse_value(detail) for col, detail in col_details}
+    goal = {}
+    for col, detail in col_details:
+        set_deep(goal, col.path, col.parse_value(detail))
+    return goal
 
 def row_to_dict(synergy_header, row):
     detail_cols = row[:len(SCHEMA)]
     synergy_cols = row[len(SCHEMA):]
 
     goal = parse_goal([(col, detail) for col, detail in zip(SCHEMA, detail_cols) if col.included])
+    goal["id"] = id_from_goal(goal)
 
     types = dict()
     subtypes = dict()
@@ -71,15 +113,18 @@ def row_to_dict(synergy_header, row):
 def rows_to_dict(header, rows):
     synergy_header = header[len(SCHEMA):]
 
-    goals = defaultdict(list)
-    goals["info"] = {"version": "v9"}
+    goals = {
+        "cardType": "difficulty-synergy",
+        "items": []
+    }
 
     for row in rows:
         try:
-            if row[0]:
+            if any(col for col in row):
                 goal = row_to_dict(synergy_header, row)
-                difficulty = goal.pop("difficulty")
-                goals[difficulty].append(goal)
+                goals["items"].append(goal)
+            else:
+                break
         except Exception as e:
             print("Exception occured when processing row:", row)
             raise e
@@ -94,7 +139,7 @@ def csv_to_json(csv_filename, json_filename):
     json_dict = rows_to_dict(header, rows)
     json_str = json.dumps(json_dict, sort_keys = True, indent = 4)
 
-    output = "var bingoList = " + json_str
+    output = json_str
     with open(json_filename, "w", encoding="utf-8") as json_file:
         json_file.write(output)
 
@@ -161,7 +206,7 @@ BASE_URL = "https://docs.google.com/spreadsheet/ccc"
 DOWNLOAD_URL = BASE_URL + "?key=1dRpwfIV2vDRL_Hq-pBj3U7wq7XwZ9JPW9Ac8hK5qbgc&output=csv"
 
 CSV_FILENAME = "goals.csv"
-JSON_FILENAME = "goal-list.js"
+JSON_FILENAME = "goal-list.json"
 
 if __name__ == "__main__":
     try:
